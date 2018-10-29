8adcf31254573f5cac2054bbc2f75a562b8828a2
SparkingR <sparkurumi@gmail.com>
2018-10-29 21:26:44 +0800

    feat: copy example from antd and make css to scss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import classNames from "classnames/bind";
+import styles from "./App.module.scss";
+import { Tree } from "antd";
+
+const cx = classNames.bind(styles);
+const TreeNode = Tree.TreeNode;
 
 class App extends Component {
+  state = {
+    treeData: [
+      { title: "Expand to load", key: "0" },
+      { title: "Expand to load", key: "1" },
+      { title: "Tree Node", key: "2", isLeaf: true }
+    ]
+  };
+
+  onLoadData = treeNode => {
+    return new Promise(resolve => {
+      if (treeNode.props.children) {
+        resolve();
+        return;
+      }
+      setTimeout(() => {
+        treeNode.props.dataRef.children = [
+          { title: "Child Node", key: `${treeNode.props.eventKey}-0` },
+          { title: "Child Node", key: `${treeNode.props.eventKey}-1` }
+        ];
+        this.setState({
+          treeData: [...this.state.treeData]
+        });
+        resolve();
+      }, 1000);
+    });
+  };
+
+  renderTreeNodes = data => {
+    return data.map(item => {
+      if (item.children) {
+        return (
+          <TreeNode title={item.title} key={item.key} dataRef={item}>
+            {this.renderTreeNodes(item.children)}
+          </TreeNode>
+        );
+      }
+      return <TreeNode {...item} dataRef={item} />;
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Tree loadData={this.onLoadData} className={cx("app")}>
+        {this.renderTreeNodes(this.state.treeData)}
+      </Tree>
     );
   }
 }
